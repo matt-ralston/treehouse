ffee0e067658d8ad40fa5fae09cd1b4a057c0b9b
MATT RALSTON <jmatt.ralston@gmail.com>
2015-08-21 16:19:00 -0700

    added a song to playlist

--- playlist/app.js
+++ playlist/app.js
@@ -2,9 +2,11 @@ var playlist = new Playlist();
 
 var hereComesTheSun = new Song("Here Comes the Sun", "The Beatles", "2:54");
 var walkingOnSunshine = new Song("Walking on Sunshine", "Katrina and the Waves", "3:11");
+var vodkaAndSoda = new Song("Vodka and Soda", "Miles Ralston", "4:15");
 
 playlist.add(hereComesTheSun);
 playlist.add(walkingOnSunshine);
+playlist.add(vodkaAndSoda);
 
 var playlistElement = document.getElementById("playlist");
 
@@ -13,14 +15,17 @@ playlist.renderInElement(playlistElement);
 var playButton = document.getElementById("play");
 playButton.onclick = function() {
   playlist.play();
+  playlist.renderInElement(playlistElement);
 }
 
 var nextButton = document.getElementById("next");
 nextButton.onclick = function() {
   playlist.next();
+  playlist.renderInElement(playlistElement);
 }
 
 var stopButton = document.getElementById("stop");
 stopButton.onclick = function() {
   playlist.stop();
+  playlist.renderInElement(playlistElement);
 }
