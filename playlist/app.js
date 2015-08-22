9097f1e98f1de754d64e31946a1c79d1626c3c37
MATT RALSTON <jmatt.ralston@gmail.com>
2015-08-21 17:25:03 -0700

    added Movie constructor object

--- playlist/app.js
+++ playlist/app.js
@@ -3,10 +3,13 @@ var playlist = new Playlist();
 var hereComesTheSun = new Song("Here Comes the Sun", "The Beatles", "2:54");
 var walkingOnSunshine = new Song("Walking on Sunshine", "Katrina and the Waves", "3:11");
 var vodkaAndSoda = new Song("Vodka and Soda", "Miles Ralston", "4:15");
+var aceVentura = new Movie("Ace Ventura", 2001, "2:10:00");
 
 playlist.add(hereComesTheSun);
 playlist.add(walkingOnSunshine);
 playlist.add(vodkaAndSoda);
+playlist.add(aceVentura);
+
 
 var playlistElement = document.getElementById("playlist");
 
